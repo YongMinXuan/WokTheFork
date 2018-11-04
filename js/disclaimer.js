bbd1626777bb21a8d32975d73112929dc6494113
Unknown <dhea23mar@gmail.com>
2018-11-04 22:08:05 +0800

    add disclaimer

---
+++ js/disclaimer.js
@@ -0,0 +1,7 @@
+/*
+Disclaimer:
+
+This website is a project from Nanyang Technological University, as a part of CS2024 - Web Design and Technologies.
+This is not used for any business purposes, only to get a good grade in our module.
+
+*/
