7c7e374aac5eb56be3c4a2ea0f023857360c7f56
max <minxuan4787@gmail.com>
2018-11-16 19:10:39 +0800

    Minified the js and added missing images

--- js/cart.js
+++ js/cart.js
@@ -1,47 +1,5 @@
-function updateTotal(){
-  var price = parseFloat(document.getElementById('tour-price').innerHTML);
-  var noOfPax = parseInt(document.getElementById('pax-input').value);
-  document.getElementById('subtotal').innerHTML = String(parseFloat(price * noOfPax).toFixed(2));
-  document.getElementById('total-cart').innerHTML = String(parseFloat(price * noOfPax).toFixed(2));
-  var totalCart = checkCouponCode();
-  console.log(totalCart);
-  document.getElementById('total-cart').innerHTML = String(totalCart.toFixed(2));
-}
-
-function checkCouponCode(){
-  var couponStatus = document.getElementById('coupon-status').innerHTML;
-  console.log(couponStatus);
-  var couponCode = document.getElementById('coupon-code').value;
-  var currentPrice = parseFloat(document.getElementById('total-cart').innerHTML);
-  if (couponCode == ""){
-    return currentPrice;
-  }
-  if (couponStatus == "Promo code applied!"){
-    return currentPrice;
-  } else {
-    var coupons = {
-      "FAM50": 0.5,
-      "DIWALI18": 0.8
-    }
-
-    var discount = coupons[couponCode];
-    if (discount == undefined){
-      document.getElementById('coupon-status').innerHTML = "Coupon is wrong please enter again";
-      return currentPrice
-    } else {
-      document.getElementById('coupon-status').innerHTML = "Promo code applied!";
-    }
-
-
-    var newPrice = currentPrice * discount;
-    document.getElementById('total-cart').innerHTML = String(newPrice.toFixed(2));
-
-
-    return newPrice;
-  }
-
-
-
-
-
-}
+function updateTotal(){var price=parseFloat(document.getElementById('tour-price').innerHTML);var noOfPax=parseInt(document.getElementById('pax-input').value);document.getElementById('subtotal').innerHTML=String(parseFloat(price*noOfPax).toFixed(2));document.getElementById('total-cart').innerHTML=String(parseFloat(price*noOfPax).toFixed(2));var totalCart=checkCouponCode();console.log(totalCart);document.getElementById('total-cart').innerHTML=String(totalCart.toFixed(2))}
+function checkCouponCode(){var couponStatus=document.getElementById('coupon-status').innerHTML;console.log(couponStatus);var couponCode=document.getElementById('coupon-code').value;var currentPrice=parseFloat(document.getElementById('total-cart').innerHTML);if(couponCode==""){return currentPrice}
+if(couponStatus=="Promo code applied!"){return currentPrice}else{var coupons={"FAM50":0.5,"DIWALI18":0.8}
+var discount=coupons[couponCode];if(discount==undefined){document.getElementById('coupon-status').innerHTML="Coupon is wrong please enter again";return currentPrice}else{document.getElementById('coupon-status').innerHTML="Promo code applied!"}
+var newPrice=currentPrice*discount;document.getElementById('total-cart').innerHTML=String(newPrice.toFixed(2));return newPrice}}
